57f9fa35cabedf699ff37d9c33fdd6f698dfda86
saurabh devade <saurabh.devade21@gmail.com>
2019-12-27 00:43:44 +0000

    session id genrated and saved to databse

--- backend/dao/AdminDAO.py
+++ backend/dao/AdminDAO.py
@@ -1,4 +1,8 @@
+import uuid
+
 import pymysql
+from sqlalchemy.dialects.mssql.information_schema import columns
+from sqlalchemy.engine import result
 
 from dbconfig import mysql
 from flask import jsonify
@@ -6,22 +10,40 @@ from models.employee import Employee
 
 
 class AdminDAO:
-    conn = mysql.connect()
-    cursor = conn.cursor(pymysql.cursors.DictCursor)
 
     @classmethod
     def adminlogin(cls, username, password):
         try:
-            admin = Employee()
-            cls.cursor.execute("SELECT * FROM employee e where e.username = %s and e.password = %s",
-                               (username, password))
-            rows = cls.cursor.fetchone()
-            resp = jsonify(rows)
-            resp.status_code = 200
-            admin = resp
-            return admin
+            conn = mysql.connect()
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+            cursor.execute("SELECT * FROM employee e where e.username = %s and e.password = %s",
+                           (username, password))
+            rows = cursor.fetchone()
+
+            return rows
         except Exception as e:
             print(e)
         finally:
-            cls.cursor.close()
-            cls.conn.close()
+            cursor.close()
+            conn.close()
+
+    @classmethod
+    def updateAdminSessionToken(cls, employeeId):
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+            sessionId = str(uuid.uuid4())
+
+            cursor.execute("update employee e set session_id = %s where employee_id = %s ",
+                           (sessionId, employeeId))
+            conn.commit()
+            cursor.execute("select * from employee e where e.employee_id = %s ",
+                           employeeId)
+            d = cursor.fetchone()
+
+            return d
+        except Exception as e:
+            print(e)
+        finally:
+            cursor.close()
+            conn.close()
--- backend/service/adminservice.py
+++ backend/service/adminservice.py
@@ -7,4 +7,7 @@ class AdminService:
     @classmethod
     def adminlogin(cls, headers, data):
         responseData = cls.adminDAO.adminlogin(data.get('username'), data.get('password'))
+
+        if responseData is not None:
+            print(cls.adminDAO.updateAdminSessionToken(responseData['employee_id']))
         return responseData
