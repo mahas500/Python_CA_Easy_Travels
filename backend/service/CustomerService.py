87f5f79721b55395d6c0171bee1411b8479ccac7
mahas500 <10518579@mydbs.ie>
2019-12-28 16:08:05 +0000

    customer enquiry creation by employee implemented
    customer enquiry creation by employee implemented

--- backend/service/CustomerService.py
+++ backend/service/CustomerService.py
@@ -1,3 +1,5 @@
+from wsgiref import headers
+
 from dao.CustomerDAO import CustomerDAO
 from dao.EmployeeDAO import EmployeeDAO
 from service.EmployeeService import EmployeeService
@@ -18,44 +20,30 @@ class CustomerService:
                 customer = cls.customerDAO.customerCreate(data.get('name'),
                                                               data.get('username'), data.get('password'),
                                                               data.get('email'), data.get('contact_no'))
-                print("hi 1")
-                print(customer['customer_id'])
-                print(customer)
-                print("hi 2")
+
         return customer;
 
 
     @classmethod
     def enquiryCreate(cls, headers, data):
-       # print(headers.get('sessionId'))
-        if cls.employeeService.checkIfEmployeeLoggedIn(headers.get('sessionId')):
-            employee = cls.employeeDAO.getEmployeeFromSessionId(headers.get('sessionId'))
-            if cls.employeeService.checkIfEmployeeHasARole(employee['employee_id'], 4):
-                print(employee['employee_id'])
-                customer1 = cls.customerDAO.customerCreate(data.get('name'),
-                                                              data.get('username'), data.get('password'),
-                                                              data.get('email'), data.get('contact_no'))
 
-                print(customer1)
-                print("hi 1")
-                print(customer1['customer_id'])
-                print(customer1)
+        if cls.employeeService.checkIfEmployeeLoggedIn(headers.get('employee_sessionId')):
+            employee = cls.employeeDAO.getEmployeeFromSessionId(headers.get('employee_sessionId'))
+            if cls.checkIfCustomerExist(headers.get('customer_sessionId')):
+                customer = cls.customerDAO.getCustomerFromCustomersessionId(headers.get('customer_sessionId'))
+                enquiry = cls.customerDAO.customerEnquiryCreation(employee['employee_id'],customer['customer_id'],
+                                                                        data.get('enquiry_detail'),data.get('enquiry_type'),
+                                                                        data.get('required_days'),data.get('required_nights'),
+                                                                        data.get('required_country'))
 
-                if (cls.checkIfCustomerExist(customer1['customer_id'])):
-                    print("hello")
-                    enquiry = cls.customerDAO.customerEnquiryCreation(employee['employee_id'],customer1['customer_id'],
-                                                                            data.get('enquiry_detail'),data.get('enquiry_type'),
-                                                                            data.get('required_days'),data.get('required_nights'),
-                                                                            data.get('required_country'))
-                print("Hello enquiry")
-                print(enquiry['enquiry_id'])
 
-        return None;
+        return enquiry;
 
 
     @classmethod
-    def checkIfCustomerExist(cls, customer_id):
-        responseData = cls.customerDAO.getCustomerFromCustomerId(customer_id)
+    def checkIfCustomerExist(cls, sessionId):
+        responseData = cls.customerDAO.getCustomerFromCustomersessionId(sessionId)
+        print(responseData)
         print("hello hey")
         print(responseData)
         if responseData is not None:
