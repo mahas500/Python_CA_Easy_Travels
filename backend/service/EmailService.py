7ab86a2328e5d69d40de6543c1d28e8f2b6c810f
saurabh devade <saurabh.devade21@gmail.com>
2019-12-30 23:53:19 +0000

    forget password with otp implemented

--- backend/service/EmailService.py
+++ backend/service/EmailService.py
@@ -14,12 +14,12 @@ class EmailService:
     def sendEmail(cls, receiver_email, message):
         try:
             print(message, receiver_email, cls.Subject)
-            # with smtplib.SMTP(cls.smtp_server, cls.port) as server:
-            #     server.ehlo()  # Can be omitted
-            #     server.starttls(context=cls.context)
-            #     server.ehlo()  # Can be omitted
-            #     server.login(cls.sender_email, cls.password)
-            #     server.sendmail(cls.sender_email, cls.Subject, receiver_email, message)
+            with smtplib.SMTP(cls.smtp_server, cls.port) as server:
+                server.ehlo()  # Can be omitted
+                server.starttls(context=cls.context)
+                server.ehlo()  # Can be omitted
+                server.login(cls.sender_email, cls.password)
+                server.sendmail(cls.sender_email, cls.Subject, receiver_email, message)
             return True
         except Exception as e:
             return False
